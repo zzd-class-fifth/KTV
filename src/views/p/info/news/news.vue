33215907594aff847b1f9fc2e710b9c9d649b179
nacy <45811149+Garfiy@users.noreply.github.com>
2021-04-20 19:58:49 +0800

    上传修改

--- src/views/p/info/news/news.vue
+++ src/views/p/info/news/news.vue
@@ -1,5 +1,44 @@
 <template>
-    <div>
-        news
-    </div>
+  <div class="news">
+    <img src="@/assets/img/bell_1.jpg" alt="" />
+    <img :src="imgUrl" alt="" />
+    <div class="bgImg"></div>
+  </div>
 </template>
+
+<script>
+// 写在这里时~就是同步加载
+import img1 from '@/img.png';
+
+export default {
+  props: {
+    id: {
+      default: 1,
+    },
+  },
+  data() {
+    return {
+        // 这样子写是懒加载的模式 
+        // 只有使用到这张图片的时候 才会加载它
+        // 网速慢的话~是会有闪烁的情况(或者没显示)
+      imgUrl: require(`@/assets/img/bell_${this.id}.jpg`),
+      //   imgUrl:
+      //     "https://2c.zol-img.com.cn/product/124_501x2000/978/cevEcNIkuOIWI.jpg",
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+.news {
+  width: 100px;
+}
+
+.bgImg {
+  /* background-image: url("https://2c.zol-img.com.cn/product/124_501x2000/978/cevEcNIkuOIWI.jpg"); */
+  background-image: url("~@/assets/img/bell_1.jpg");
+  width: 200px;
+  height: 200px;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,25 @@
+const path = require('path')
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? './' : './'
+    publicPath: process.env.NODE_ENV === 'production' ? './' : './',
+
+    chainWebpack: config => {
+        const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+        // 这里修改 sylus 为 less
+        types.forEach(type => addStyleResource(config.module.rule('less').oneOf(type)));
+    },
+}
+
+function addStyleResource(rule) {
+    rule.use('style-resource')
+        .loader('style-resources-loader')
+        .options({
+            patterns: [
+                // 这里修改为所需要全局引入的less文件的路径
+                // __dirname 这个是获取当前更目录的地址
+                // path.resolve(__dirname, './src/styles/imports.styl'),
+
+                path.resolve(__dirname, './src/assets/less/common.less'),
+            ],
+        })
 }
